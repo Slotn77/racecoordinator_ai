09064ad529846e3b4889f84f6336ac204cfcab5c
Dave <contact@racecoordinator.net>
2026-05-11 00:21:03 -0400

    Version number bump to v0.18

--- client/src/app/components/raceday-setup/raceday-setup.component.ts
+++ client/src/app/components/raceday-setup/raceday-setup.component.ts
@@ -79,7 +79,7 @@ export class RacedaySetupComponent implements OnInit, OnDestroy {
   tempServerPort = 7070;
   serverIp: string = "";
   serverVersion: string = "";
-  clientVersion: string = (window as any).CLIENT_VERSION_OVERRIDE || "0.0.0.17";
+  clientVersion: string = (window as any).CLIENT_VERSION_OVERRIDE || "0.0.0.18";
   showAboutDialog = false;
 
   scale: number = 1;
