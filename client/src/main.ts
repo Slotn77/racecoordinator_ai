c705b603a5caf2895d98fa4fc40ac655e4b3c299
Dave <contact@racecoordinator.net>
2026-06-12 11:03:37 -0400

    Cancelling navigation no longer removes the navigation from the browser history https://github.com/daufderheide/racecoordinator_ai/issues/355

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,7 +2,7 @@ import "@angular/compiler";
 import { provideHttpClient, withInterceptors } from "@angular/common/http";
 import { bootstrapApplication } from "@angular/platform-browser";
 import { provideAnimations } from "@angular/platform-browser/animations";
-import { provideRouter } from "@angular/router";
+import { provideRouter, withRouterConfig } from "@angular/router";
 import { authInterceptor } from "@app/services/auth.interceptor";
 import { LoggerService } from "@app/services/logger.service";
 import { RaceService } from "@app/services/race.service";
@@ -12,7 +12,12 @@ import { routes } from "./app/app.routes";
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routes),
+    provideRouter(
+      routes,
+      withRouterConfig({
+        canceledNavigationResolution: "computed",
+      }),
+    ),
     provideAnimations(),
     provideHttpClient(withInterceptors([authInterceptor])),
     RaceService,
